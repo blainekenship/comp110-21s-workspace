b97617d0527fc244b93d844d913a025353f02aa3
Blaine Jenkins <blaine28@email.unc.edu>
2021-02-12 21:06:38 -0500

    Progress on Exercise 2

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -9,10 +9,26 @@ def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
     # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 
 # TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str:
+    """A fortune cookie"""
+    fortune: int = randint(1,4)
+
+    if fortune == 1:
+        return("An unlit candle frightens no monkeys")
+    else:
+        if fortune == 2:
+            return("Today you will surpass all others")
+        else:
+            if fortune == 3:
+                return("I have a dream... time to go to bed!")
+            else:
+                return("Those who do not seek do not find")
+
 
 
 # Python Idiom for "starting" the program when run as a module.
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -6,11 +6,21 @@ __author__ = "YOUR 9-DIGIT PID"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
-
+    print(tar_heels(choice))
 
 # TODO 1: Define the tar_heels function, and its logic, here.
-
+def tar_heels(a: int) -> str:
+    """A pro-Carolina program"""
+    if a % 2 == 0 and a % 7 == 0:
+        return("TAR HEELS")
+    else:
+        if a % 2 == 0:
+            return("TAR")
+        else:
+            if a % 7 == 0:
+                return("HEELS")
+            else:
+                return("CAROLINA")
 
 if __name__ == "__main__":
     main()
