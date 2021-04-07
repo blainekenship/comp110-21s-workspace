b1aad1b2f7c93232789016775f3d9f9336b23f8f
Blaine Jenkins <blaine28@email.unc.edu>
2021-04-07 11:07:49 -0400

    work that gotta be pushed

---
+++ exercises/quiz_work3.py
@@ -0,0 +1,14 @@
+"""Work on Quiz, Question 10"""
+
+a: list[int] = [10, 20, 30]
+b: list[int] = [30, 40, 50]
+c: list[int] = []
+
+for i in a:
+    while i < len(a):
+        c.append(i)
+        
+        
+
+
+print(c)
---
+++ lessons/classes_objects.py
@@ -0,0 +1,18 @@
+"""Examples of object-oriented programming concepts"""
+
+class Point:
+
+    # Defining attributes (related variables)
+    # of our class
+    x: float = 0.0
+    y: float = 0.0
+
+
+
+a: Point = Point()
+a.x = 4.0
+a.y = 5.0
+
+b: Point = Point()
+a.x = 2.0
+a.y = -1.0 
